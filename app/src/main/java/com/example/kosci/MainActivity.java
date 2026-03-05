e7f33bfb13404efd5c2b457f1ab9a44de93ffebb
wlutowski <lutowski.wikto@szkola.elektrykplcok.ud.pl>
2026-03-05 10:33:33 +0100

    Premiera

---
+++ app/src/main/java/com/example/kosci/MainActivity.java
@@ -0,0 +1,129 @@
+package com.example.kosci;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Random rand;
+    private TextView resultTextView;
+    private TextView singleResultTextView;
+    private ImageView[] images;
+
+    private final String singleResultText = "Wynik tego losowania: ";
+    private final String resultText = "Wynik gry: ";
+
+    private int result = 0;
+    private int singleResult = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        rand = new Random();
+
+        resultTextView = findViewById(R.id.resultTextView);
+        singleResultTextView = findViewById(R.id.singleResultTextView);
+
+        images = new ImageView[5];
+
+        images[0] = findViewById(R.id.image1);
+        images[1] = findViewById(R.id.image2);
+        images[2] = findViewById(R.id.image3);
+        images[3] = findViewById(R.id.image4);
+        images[4] = findViewById(R.id.image5);
+
+        findViewById(R.id.throwButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HashSet<Integer> throwenNumbers = new HashSet<Integer>();
+                ArrayList<Integer> choosenNumbers = new ArrayList<Integer>();
+
+                singleResult = 0;
+
+                for (int i = 0; i < images.length; i++)
+                {
+                    int randInt = rand.nextInt(6);
+
+                    if(throwenNumbers.contains(randInt))
+                    {
+                        if(!choosenNumbers.contains(randInt))
+                            singleResult += randInt + 1;
+
+                        choosenNumbers.add(randInt);
+                        singleResult += randInt + 1;
+                    }
+                    else
+                        throwenNumbers.add(randInt);
+
+
+                    switch (randInt)
+                    {
+                        case 0:
+                            images[i].setImageResource(R.drawable.k1);
+                            break;
+                        case 1:
+                            images[i].setImageResource(R.drawable.k2);
+                            break;
+                        case 2:
+                            images[i].setImageResource(R.drawable.k3);
+                            break;
+                        case 3:
+                            images[i].setImageResource(R.drawable.k4);
+                            break;
+                        case 4:
+                            images[i].setImageResource(R.drawable.k5);
+                            break;
+                        case 5:
+                            images[i].setImageResource(R.drawable.k6);
+                            break;
+                    }
+
+                }
+
+                result += singleResult;
+
+                resultTextView.setText(resultText + result);
+                singleResultTextView.setText(singleResultText + singleResult);
+
+            }
+        });
+
+        findViewById(R.id.resetButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                for (int i = 0; i < images.length; i++)
+                {
+                    images[i].setImageResource(R.drawable.question);
+                }
+
+                singleResult = 0;
+                result = 0;
+
+                resultTextView.setText(resultText + result);
+                singleResultTextView.setText(singleResultText + singleResult);
+            }
+        });
+    }
+}
